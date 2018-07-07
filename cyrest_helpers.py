270f629cc93580cc9e49071f7d8ab2ab50bfcfa4
Zichen Wang <wangzc921@gmail.com>
2018-07-07 10:11:14 -0400

    Moved util functions to .py script and added AE models

---
+++ cyrest_helpers.py
@@ -0,0 +1,84 @@
+# modules to interact with CyREST, the API of Cytoscape
+import requests
+from py2cytoscape.data.cyrest_client import CyRestClient
+import py2cytoscape.cytoscapejs as renderer
+
+
+# Params for CyREST
+IP = '127.0.0.1'
+PORT = 1234
+BASE_URL = 'http://%s:%s/v1' % (IP, PORT)
+HEADERS = {'Content-Type': 'application/json'}
+
+def network_force_directed_layout(G):
+    '''POST the network to CyREST, then run layout algorithm, 
+    finally return the coordinates and the cy_network.
+    '''
+    # Create Py2cytoscape client
+    cy = CyRestClient(ip=IP, port=PORT)
+    # Reset session
+    cy.session.delete()
+    # POST the graph to CyREST
+    G_cy = cy.network.create_from_networkx(G)
+
+    # Change the layout params
+    layout_parameters = [
+        {"name": "numIterations", "value": 500}, 
+        {"name":"numIterationsEdgeRepulsive", "value":500}
+    ]
+    resp = requests.put(BASE_URL+ '/apply/layouts/force-directed-cl/parameters', 
+                 data=json.dumps(layout_parameters),
+                 headers=HEADERS)
+    cy.layout.apply(name='force-directed-cl', network=G_cy)
+
+    # Get current view
+    view = G_cy.get_first_view()
+    nodes = view['elements']['nodes']
+    # Get a coord matrix ordered by id_original
+    ids_original = np.array([n['data']['id_original'] for n in nodes]).astype(np.int32)
+    xs = [n['position']['x'] for n in nodes]
+    ys = [n['position']['y'] for n in nodes]
+    coords = np.array([xs, ys]).T
+    return coords[ids_original], G_cy
+    
+
+def network_firework_layout(G):
+    '''POST the network to CyREST, then run layout algorithm, 
+    finally return the coordinates and the cy_network.
+    '''
+    # Create Py2cytoscape client
+    cy = CyRestClient(ip=IP, port=PORT)
+    # Reset session
+    cy.session.delete()
+    # POST the graph to CyREST
+    G_cy = cy.network.create_from_networkx(G)
+
+    # Change the layout params
+    layout_parameters = [
+        {"name": "numIterations", "value": 10}, 
+        {"name":"numIterationsEdgeRepulsive", "value":10}
+    ]
+    resp = requests.put(BASE_URL+ '/apply/layouts/force-directed-cl/parameters', 
+                 data=json.dumps(layout_parameters),
+                 headers=HEADERS)
+    cy.layout.apply(name='force-directed-cl', network=G_cy)
+
+
+    layout_parameters = [
+        {"name": "maxIterations", "value": 10000}, 
+        {"name":"randomize", "value":True},
+    ]
+    resp = requests.put(BASE_URL+ '/apply/layouts/allegro-edge-repulsive-strong-clustering/parameters', 
+                 data=json.dumps(layout_parameters),
+                 headers=HEADERS)
+    cy.layout.apply(name='allegro-edge-repulsive-strong-clustering', network=G_cy)
+    # Get current view
+    view = G_cy.get_first_view()
+    nodes = view['elements']['nodes']
+    # Get a coord matrix ordered by id_original
+    ids_original = np.array([n['data']['id_original'] for n in nodes]).astype(np.int32)
+    xs = [n['position']['x'] for n in nodes]
+    ys = [n['position']['y'] for n in nodes]
+    coords = np.array([xs, ys]).T
+    return coords[ids_original], G_cy
+    
---
+++ utils.py
@@ -0,0 +1,91 @@
+'''Util functions for DR
+'''
+from collections import Counter, OrderedDict
+
+import numpy as np
+import networkx as nx
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from sklearn.metrics.pairwise import pairwise_distances
+
+from cyrest_helpers import *
+
+def plot_embed(X_coords, labels, figsize=(6, 6)):
+    '''Scatter plot for the coordinates colored by their labels'''
+    X_coords = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit_transform(X_coords[:, :2])
+
+    fig, ax = plt.subplots(figsize=figsize)
+    scatter_proxies = []
+    colors = [COLORS10[l] for l in labels]
+    ax.scatter(X_coords[:, 0], X_coords[:, 1], s=5, c=colors, edgecolor='none')
+    for i in range(10):
+        scatter_proxy = Line2D([0],[0], ls="none", 
+                               c=COLORS10[i], 
+                               marker='o', 
+                               markersize=5, 
+                               markeredgecolor='none')    
+        scatter_proxies.append(scatter_proxy)
+    
+    # Shrink current axis by 20%
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # Put a legend to the right of the current axis
+    ax.legend(scatter_proxies, map(str, range(10)), 
+              numpoints=1,
+              loc='center left', bbox_to_anchor=(1, 0.5))
+
+    fig.tight_layout()
+    return ax
+
+  
+def compute_adjcency_mat(X, metric='euclidean'):
+    pdist = pairwise_distances(X, metric=metric)
+    adj_mat = 1 - pdist / pdist.max()
+    # remove 1's on the diagnal
+    adj_mat -= np.eye(X.shape[0])
+    return adj_mat
+
+
+def plot_degree_distribution(G):
+    """G: a nx.Graph object
+    """
+    fig, ax = plt.subplots(figsize=(5,5))
+    
+    degrees = G.degree().values()
+    degrees = dict(Counter(degrees))
+    x = degrees.keys()
+    y = degrees.values()
+
+    ax.scatter(x, y, s=10, alpha=.6)
+    ax.set_xlabel('Degree', fontsize=12)
+    ax.set_ylabel('Count', fontsize=12)    
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    fig.tight_layout()
+    return ax
+    
+
+def create_knn_graph(X, k=30, metric='euclidean'):
+    '''Create a graph from a data matrix (sample x features).
+    '''
+    adj_mat = neighbors.kneighbors_graph(X, k, mode='connectivity', metric=metric)
+    G = nx.from_scipy_sparse_matrix(adj_mat)
+    return G
+
+# thresholding-Firework
+def create_graph_by_threshold(adj_mat, percentile):
+    triu_idx = np.tril_indices(adj_mat.shape[0], 1)
+    thresold = np.percentile(adj_mat[triu_idx], percentile)
+    adj_mat_ = adj_mat.copy()
+    adj_mat_[adj_mat<thresold] = 0
+    G = nx.from_numpy_matrix(adj_mat_)
+    return G
+
+def create_graph_by_threshold_knn(adj_mat, percentile, k=1, X=None):
+    '''combine the graph from `create_graph_by_threshold` with a kNN graph.
+    '''
+    G_thres = create_graph_by_threshold(adj_mat, percentile)
+    G_knn = create_knn_graph(X, k=k)
+    return nx.compose(G_thres, G_knn)
+
